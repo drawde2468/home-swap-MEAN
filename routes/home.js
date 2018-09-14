40e963d2c7a7e2f44d93af4cd73b8996766ad53c
Nicholas Kreps <nicholas.kreps@gmail.com>
2018-09-14 14:50:23 -0400

    fixed add home route for saving multiple home images

--- routes/home.js
+++ routes/home.js
@@ -68,6 +68,7 @@ router.get("/myhome/:id", (req, res, next) => {
 
 // route for user to add a home to their account
 router.post("/myhome", parser, (req, res, next) => {
+  // console.log("IM CREATING A NEW HOUSE !!!!");
   if (req.isAuthenticated()) {
     const owner = req.user._id;
     // console.log(req.body);
@@ -75,13 +76,10 @@ router.post("/myhome", parser, (req, res, next) => {
     // console.log(parsedAddress);
 
     const file = req.files;
-    let images = [];
-
-    // to see what is returned to you
-    // console.log(req.files);
+    const images = [];
 
     for (i = 0; i < file.length; i++) {
-      let img = {
+      const img = {
         url: file[i].url,
         id: file[i].id
       };
@@ -98,29 +96,70 @@ router.post("/myhome", parser, (req, res, next) => {
       country: parsedAddress.country
     };
 
-    const userHome = new Home({
-      owner,
-      home,
-      setting,
-      landscape,
-      bedrooms,
-      beds,
-      baths,
-      address,
-      description,
-      images
-    });
-
-    userHome
-      .save()
-      .then(userHome => {
-        res.json({
-          message: "New Home Added!"
-        });
+    Home.find({$and:[{'address.street': address.street},{'address.zipCode': address.zipCode}]})
+      .then((userHomes, err) => {
+        if (err) {
+          console.log('HOUSE CREATION ERROR');
+          res.json(err);
+          return;
+        }
+        if (userHomes.length) {
+          const homeId = userHomes[0]._id;
+          // console.log('homeId ', homeId, userHomes);
+          userHomes[0].images.push(images[0]);
+          console.log('UPDATED HOUSE ', userHomes);
+          Home.findByIdAndUpdate(homeId, userHomes[0], { new: true })
+          .then(home => {
+            console.log("I was updated"); 
+            // return;
+        //     return res.json({
+        //     message: "Your home was updated successfully"
+        // });
       })
       .catch(error => next(error));
+      return;
+        }
+        else{
+          console.log('HOUSE CREATION HOUSE DOES NOT EXIST');
+          const userHome = new Home({
+            owner,
+            home,
+            setting,
+            landscape,
+            bedrooms,
+            beds,
+            baths,
+            address,
+            description,
+            images
+          });
+      
+          userHome
+            .save()
+            .then(userHome => {
+              console.log("FOUND YOUR HOUSE IN THE DB!!!!");
+            })
+            .catch(error => console.log("ERROR LOOKING FOR HOUSE"));
+      
+          return;
+        }
+        //console.log(res.json(userHomes));
+      })
+      .catch(error => next(error));
+    
+    
+    // to see what is returned to you
+    // console.log(req.files);
 
-    return;
+    // const { home, setting, landscape, bedrooms, beds, baths, description } = req.body;
+
+    // const address = {
+    //   street: parsedAddress.street,
+    //   city: parsedAddress.city,
+    //   state: parsedAddress.state,
+    //   zipCode: parsedAddress.zipCode,
+    //   country: parsedAddress.country
+    // };
   }
 
   res.status(403).json({
