3f8f1dca0152b008f76b4cff55c04b168bf633dd
Edward Leslie <edwardleslie09@gmail.com>
2018-09-14 22:29:59 -0400

    heroku nonsense part 2

--- app.js
+++ app.js
@@ -108,6 +108,10 @@ app.use('/api', myAccountRouter);
 
 app.use('/', indexRouter);
 
+app.use(function(req, res) {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
